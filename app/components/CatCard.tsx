22a4ee0dd07fd0c59f25c49cbb1b07cf49cb736e
MPonomar <mponomar96@gmail.com>
2025-08-04 18:40:14 +0200

    fix close btn

--- app/components/CatCard.tsx
+++ app/components/CatCard.tsx
@@ -46,11 +46,11 @@ export default function CatCard({ cat, onRemove}: CatCardProps) {
 
   return (
     <div
-      className="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 hover:-translate-y-1 transform transition-transform"
+      className="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 hover:-translate-y-1 transform transition-transform relative"
     >
       <button
         type="button"
-        className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 absolute z-1000 right-px"
+        className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 absolute z-1000 top-4 right-4 cursor-pointer"
         data-dismiss-target="#toast-default"
         aria-label="Close"
         onClick={onRemove}
